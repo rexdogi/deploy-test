8d0a56d9805956fe1812729fa2a69dfa69e09d69
Paulius Mikalauskas <paulius@invoice123.com>
2024-09-27 16:07:15 +0300

    TEST

--- .docker/php/bin/run.sh
+++ .docker/php/bin/run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Wait for MySQL to be available
+until nc -z -v -w30 db 3306
+do
+  echo "Waiting for database connection..."
+  sleep 1
+done
+
 php artisan migrate
 
-php-fpm
+exec php-fpm
